49dfc1241f9ad0113d385beb8fc4606b544f4b56
andreacassini <andrea1316@hotmail.it>
2023-07-29 15:32:54 +0200

    migrate controller and model

--- database/migrations/2023_07_27_074345_create_trains_table.php
+++ database/migrations/2023_07_27_074345_create_trains_table.php
@@ -15,6 +15,18 @@ return new class extends Migration
     {
         Schema::create('trains', function (Blueprint $table) {
             $table->id();
+            $table->string('azienda', 40);
+            $table->string('stazione_partenza', 100);
+            $table->string('stazione_arrivo', 100);
+            $table->date('data_partenza');
+            $table->date('data_arrivo');
+            $table->time('orario_partenza');
+            $table->time('orario_arrivo');
+            $table->string('codice_treno', 7);
+            $table->integer('n_carrozze');
+            $table->boolean('in_orario')->default(false);
+            $table->time('tempo_ritardo')->nullable();
+            $table->boolean('cancellato')->default(false);
             $table->timestamps();
         });
     }
