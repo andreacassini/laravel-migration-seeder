6a157f20728c8a60b6f974fa0f6f3963b08df912
andreacassini <andrea1316@hotmail.it>
2023-07-29 16:16:09 +0200

    style es done

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -19,12 +19,32 @@
 
     <main>
         <div class="container">
-            <div class="row">
-                <div class="col-12">
-                    @foreach($trains as $train)
-                    {{$train->azienda}} - {{$train->codice_treno}}
-                    @endforeach
-                </div>
+            <div class="row my-5">
+                @foreach($trains as $train)
+                <div class="card mx-4" style="width: 18rem;">
+                    <div class="card-body">
+                        <h2 class="card-title"><strong>{{$train->azienda}}</strong></h2>
+                        <h4 class="card-text"><strong>Stazione partenza: </strong>{{$train->stazione_partenza}}</h4>
+                        <h4 class="card-text"><strong>Stazione arrivo: </strong>{{$train->stazione_arrivo}}</h4>
+                    </div>
+                    <ul class="list-group list-group-flush">
+                        <li class="list-group-item"><strong>Codice treno: </strong>{{$train->codice_treno}}</li>
+                        <li class="list-group-item"><strong>Orario partenza: </strong>{{$train->orario_partenza}}
+                            <li class="list-group-item"><strong>Orario arrivo: </strong>{{$train->orario_arrivo}}
+                        </li>
+                        <li class="list-group-item">
+                            @if(($train->tempo_ritardo) === NULL)
+                                <p>Il treno non ha ritardo</p>
+                            @else
+                                {{$train->tempo_ritardo.' minuti'}}</li>
+                            @endif
+                    </ul>
+                    <div class="card-body">
+                        <p><strong>Data di partenza: </strong>{{$train->data_partenza}}</p>
+                        <p><strong>Data di arrivo: </strong>{{$train->data_arrivo}}</p>
+                    </div>
+                    </div>
+                @endforeach
             </div>
         </div>
     </main>
