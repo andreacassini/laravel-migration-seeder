ea5bebaa81d351f1ac3be4347648ff24a840f924
andreacassini <andrea1316@hotmail.it>
2023-07-30 15:14:27 +0200

    installed fakerphp

---
+++ database/seeders/TrainsTableSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
+use Faker\Provider\en_US\Person;
+
+use App\Models\Train;
+
+class TrainsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run(Faker $faker)
+    {
+        for ($i = 0; $i < 10; $i++) {
+            $train = new Train();
+        }
+    }
+}
