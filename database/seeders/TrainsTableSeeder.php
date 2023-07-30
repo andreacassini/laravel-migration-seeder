f6e652c6f2ddc0358b714b1214201d01c5a43eab
andreacassini <andrea1316@hotmail.it>
2023-07-30 15:25:13 +0200

    es base done seeder fatto

--- database/seeders/TrainsTableSeeder.php
+++ database/seeders/TrainsTableSeeder.php
@@ -19,7 +19,25 @@ class TrainsTableSeeder extends Seeder
     public function run(Faker $faker)
     {
         for ($i = 0; $i < 10; $i++) {
+
+            //ISTANZA CLASSE MODEL
             $train = new Train();
+
+            $train->azienda = $faker->word();
+            $train->stazione_partenza = $faker->word();
+            $train->stazione_arrivo = $faker->word();
+            $train->data_partenza = $faker->date();
+            $train->data_arrivo = $faker->date();
+            $train->orario_partenza = $faker->time();
+            $train->orario_arrivo = $faker->time();
+            $train->codice_treno = $faker->bothify('nt???##');
+            $train->n_carrozze = $faker->numberBetween(1, 15);
+            $train->in_orario = $faker->boolean();
+            $train->tempo_ritardo = $faker->time();
+            $train->cancellato = $faker->boolean();
+
+            //SALVO ISTANZA CREATA DELLA CLASSE DEL DATABASE
+            $train->save();
         }
     }
 }
